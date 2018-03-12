dcf86588285ddfbfbbe34f743466183ea09b13f3
wearelegion8607 <rudikoff07@gmail.com>
2018-03-12 21:15:15 +0300

    full customization gulp

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Serg on 11.03.2018.
+ */
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    browserSync = require("browser-sync");
+
+gulp.task("sass",function(){
+    return gulp.src('app/sass/**/*.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync',function(){
+    browserSync({
+        server:{
+            baseDir: 'app'
+        },
+        notify: false
+    })
+});
+gulp.task('watch',['browser-sync','sass'],function(){
+    gulp.watch('app/sass/**/*.sass', ['sass']);
+    gulp.watch('app/*.html',browserSync.reload);
+    gulp.watch('app/*.html',browserSync.reload);
+});
+
+
